365cb7fc7f9b0609c8d60135232a5e769c2397d9
whydumb <108032052+whydumb@users.noreply.github.com>
2025-06-03 00:53:58 +0900

    Add files via upload

---
+++ common/src/main/java/com/kAIS/KAIMyEntity/renderer/ShaderProvider.java
@@ -0,0 +1,67 @@
+package com.kAIS.KAIMyEntity.renderer;
+
+import java.io.FileInputStream;
+import net.minecraft.client.Minecraft;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.lwjgl.opengl.GL46C;
+
+public class ShaderProvider {
+    public static final Logger logger = LogManager.getLogger();
+    private static final Minecraft MCinstance = Minecraft.getInstance();
+    private static boolean isInited = false;
+    private static int program = 0;
+    private static final String vertexPath = MCinstance.gameDirectory.getAbsolutePath() + "/KAIMyEntity/Shader/MMDShader.vsh";
+    private static final String fragPath = MCinstance.gameDirectory.getAbsolutePath() + "/KAIMyEntity/Shader/MMDShader.fsh";
+
+    public static void Init() {
+        if (!isInited) {
+            try {
+                int vertexShader = GL46C.glCreateShader(GL46C.GL_VERTEX_SHADER);
+                try (FileInputStream vertexSource = new FileInputStream(vertexPath)) {
+                    GL46C.glShaderSource(vertexShader, new String(vertexSource.readAllBytes()));
+                }
+
+                int fragShader = GL46C.glCreateShader(GL46C.GL_FRAGMENT_SHADER);
+                try (FileInputStream fragSource = new FileInputStream(fragPath)) {
+                    GL46C.glShaderSource(fragShader, new String(fragSource.readAllBytes()));
+                }
+
+                GL46C.glCompileShader(vertexShader);
+                if (GL46C.glGetShaderi(vertexShader, GL46C.GL_COMPILE_STATUS) == GL46C.GL_FALSE) {
+                    String log = GL46C.glGetShaderInfoLog(vertexShader, 8192).trim();
+                    logger.error("Failed to compile shader {}", log);
+                    GL46C.glDeleteShader(vertexShader);
+                }
+
+                GL46C.glCompileShader(fragShader);
+                if (GL46C.glGetShaderi(fragShader, GL46C.GL_COMPILE_STATUS) == GL46C.GL_FALSE) {
+                    String log = GL46C.glGetShaderInfoLog(fragShader, 8192).trim();
+                    logger.error("Failed to compile shader {}", log);
+                    GL46C.glDeleteShader(fragShader);
+                }
+                program = GL46C.glCreateProgram();
+                GL46C.glAttachShader(program, vertexShader);
+                GL46C.glAttachShader(program, fragShader);
+                GL46C.glLinkProgram(program);
+                if (GL46C.glGetProgrami(program, GL46C.GL_LINK_STATUS) == GL46C.GL_FALSE) {
+                    String log = GL46C.glGetProgramInfoLog(program, 8192);
+                    logger.error("Failed to link shader program\n{}", log);
+                    GL46C.glDeleteProgram(program);
+                    program = 0;
+                }
+                logger.info("MMD Shader Initialize finished");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            isInited = true;
+        }
+    }
+
+    public static int getProgram() {
+        if (program <= 0)
+            throw new Error("Call Shader before init");
+        return program;
+    }
+}
